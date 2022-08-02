0adafb8740d3e6c099993cfb56a7585e6dc4f19c
Eternita-S <5073202+Eternita-S@users.noreply.github.com>
2022-08-02 23:36:42 +0300

    Add project files.

---
+++ ObjectExplorer/Gui.cs
@@ -0,0 +1,22 @@
+﻿using Dalamud.Interface.Windowing;
+using ImGuiNET;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectExplorer
+{
+    internal class Gui : Window
+    {
+        public Gui() : base(P.Name)
+        {
+        }
+
+        public override void Draw()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ObjectExplorer/ObjectExplorer.cs
@@ -0,0 +1,27 @@
+﻿using Dalamud.Interface.Windowing;
+using Dalamud.Plugin;
+using ECommons;
+using System;
+
+namespace ObjectExplorer
+{
+    public class ObjectExplorer : IDalamudPlugin
+    {
+        public string Name => "ObjectExplorer";
+        internal ObjectExplorer P;
+        internal WindowSystem ws;
+        internal Gui gui;
+
+        public ObjectExplorer(DalamudPluginInterface pi)
+        {
+            P = this;
+            ECommons.ECommons.Init(pi, Module.ObjectFunctions);
+        }
+
+        public void Dispose()
+        {
+
+            P = null;
+        }
+    }
+}
